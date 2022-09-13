71fb141a8a4c2bbd065acec3296d9a095197333c
lola-source <99507617+lola-source@users.noreply.github.com>
2022-09-13 22:29:43 +0300

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,12 @@
--- prepares a MySQL server for the project
-
+-- Configuration database for work
+-- Create database hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+-- Create user of database
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+-- set password to user
+SET PASSWORD FOR 'hbnb_test'@'localhost' = PASSWORD('hbnb_test_pwd');
+-- give all the user's properties to the database
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- Let the user see the perfomance data
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
