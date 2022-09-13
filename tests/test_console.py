ddd89787d06403e3b12e6c276457ae8e5057f80a
lola-source <99507617+lola-source@users.noreply.github.com>
2022-09-13 22:42:32 +0300

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,41 +1,71 @@
 #!/usr/bin/python3
-"""
-Contains the class TestConsoleDocs
+""" test console
 """
 
-import console
-import inspect
-import pep8
 import unittest
-HBNBCommand = console.HBNBCommand
+import sys
+import pep8
+from console import HBNBCommand
+from unittest.mock import create_autospec
 
 
-class TestConsoleDocs(unittest.TestCase):
-    """Class for testing documentation of the console"""
-    def test_pep8_conformance_console(self):
-        """Test that console.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['console.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+class TestConsole(unittest.TestCase):
+    """ class testing console
+    """
+    def test_amenity_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0)
 
-    def test_pep8_conformance_test_console(self):
-        """Test that tests/test_console.py conforms to PEP8."""
-        pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_console.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+    def test_docstrings_in_console(self):
+        """checking for docstrings"""
+        """ self.assertIsNotNone(console.__doc__) """
+        self.assertIsNotNone(HBNBCommand.preloop.__doc__)
+        self.assertIsNotNone(HBNBCommand.precmd.__doc__)
+        self.assertIsNotNone(HBNBCommand.postcmd.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_create.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_show.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_destroy.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_all.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_update.__doc__)
+        """self.assertIsNotNone(HBNBCommand.count.__doc__)
+        self.assertIsNotNone(HBNBCommand.strip_clean.__doc__)
+        self.assertIsNotNone(HBNBCommand.default.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_quit.__doc__)"""
+        self.assertIsNotNone(HBNBCommand.help_quit.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
+        self.assertIsNotNone(HBNBCommand.help_EOF.__doc__)
+        self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
+        self.assertIsNotNone(HBNBCommand.help_create.__doc__)
+        self.assertIsNotNone(HBNBCommand.help_show.__doc__)
+        self.assertIsNotNone(HBNBCommand.help_destroy.__doc__)
+        self.assertIsNotNone(HBNBCommand.help_all.__doc__)
+        self.assertIsNotNone(HBNBCommand.help_count.__doc__)
+        self.assertIsNotNone(HBNBCommand.help_update.__doc__)
 
-    def test_console_module_docstring(self):
-        """Test for the console.py module docstring"""
-        self.assertIsNot(console.__doc__, None,
-                         "console.py needs a docstring")
-        self.assertTrue(len(console.__doc__) >= 1,
-                        "console.py needs a docstring")
+    def setUp(self):
+        """ standard setUp """
+        self.mock_stdin = create_autospec(sys.stdin)
+        self.mock_stdout = create_autospec(sys.stdout)
 
-    def test_HBNBCommand_class_docstring(self):
-        """Test for the HBNBCommand class docstring"""
-        self.assertIsNot(HBNBCommand.__doc__, None,
-                         "HBNBCommand class needs a docstring")
-        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
-                        "HBNBCommand class needs a docstring")
+    def create(self, server=None):
+        """ create """
+        return HBNBCommand(stdin=self.mock_stdin, stdout=self.mock_stdout)
+
+    """     def test_exit(self):
+    test exit command
+    cli = self.create()
+    self.assertTrue(cli.onecmd("quit"))
+    self.assertTrue(cli.onecmd("EOF")) """
+
+    def _last_write(self, nr=None):
+        """:return: last `n` output lines"""
+        if nr is None:
+            return self.mock_stdout.write.call_args[0][0]
+        return "".join(map(lambda c: c[0][0],
+                           self.mock_stdout.write.call_args_list[-nr:]))
+
+
+if __name__ == "__main__":
+    unittest.main()
