d08680e3304232aa34a80fa856a57a0f5272544b
lola-source <99507617+lola-source@users.noreply.github.com>
2022-09-13 22:28:44 +0300

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,12 @@
--- prepares a MySQL server for the project
-
+-- Configuration database for work
+-- Create database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
+-- Create user of database
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+-- set password to user
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = PASSWORD('hbnb_dev_pwd');
+-- give all the user's properties to the database
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Let the user see the perfomance data
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
