01cda79744b11efb02f2921c69a3631c56cea9bb
lola-source <adagaladolores@gmail.com>
2022-09-13 20:32:14 +0000

    files

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,12 +1,7 @@
--- Configuration database for work
--- Create database hbnb_dev_db
+-- prepares a MySQL server for the project
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- Create user of database
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
--- set password to user
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = PASSWORD('hbnb_dev_pwd');
--- give all the user's properties to the database
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- Let the user see the perfomance data
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
